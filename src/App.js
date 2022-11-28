0e82ee45d3900b72487e0e04b85f5eb22f4e2b94
Ashutosh Rattan <ashutoshrattan4@gmail.com>
2022-11-28 17:25:50 +0530

    complete basic app.js with router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
-  <>
-  </>
+    <Router>
+      <Routes>
+        {/* <Route path='/' element={<Home />} /> */}
+      </Routes>
+    </Router>
   )
 }
 
